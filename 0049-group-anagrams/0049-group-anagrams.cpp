31d1e2f9cf3fc3bdbdb777ebc5c2f8ed96783348
Lav Yadav <yadavlav571@gmail.com>
2026-07-28 01:10:29 +0530

    Time: 194 ms (5.06%), Space: 86.8 MB (5.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n=strs.size();
+
+        unordered_map<string,vector<string>>mp;
+
+        for(int i=0;i<n;i++){
+
+            vector<int>freq(26,0);
+
+            for(auto c : strs[i]){
+                freq[c-'a']++;
+            }
+
+            string key="";
+
+            for(auto it:freq){
+                key=key+"#"+to_string(it);
+            }
+
+            mp[key].push_back(strs[i]);
+        }
+
+        vector<vector<string>>ans;
+
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+        
+    }
+};
